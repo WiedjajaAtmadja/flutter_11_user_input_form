27b282204d250848510dbf5fd2e69977497ed01d
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2023-06-05 16:16:58 +0700

    Step 1. Initial code

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_11_user_input_form
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_11_user_input_form/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_11_user_input_form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
